af4ff93a027f25a15a4afcfe083f3768446f330c
Joakim66 <Joakim.Solbak96@gmail.com>
2020-05-31 01:00:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+  name: "Whiskers",
+  color: "white",
+  age: 1
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+var catArray = [catObject];
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++){
+  console.log(catArray);
+}
+
+// question 9
+
+var logToConsole = function(parameter1){
+  console.log(parameter1);
+}
+
+// question 10
+
+logToConsole("donkey");
